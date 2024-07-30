d59c5174109964ec6f03200e5dab1c5d186611d0
arfaniakbar <akbarhansem01@gmail.com>
2024-07-30 21:16:29 +0700

    add : penambahan fungsi

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -3,8 +3,29 @@
 #include <string>
 using namespace std;
 
-int main() {
+// Definisi struct untuk menyimpan data lari
+struct DataLari {
+    string nama;
+    string jenisKelamin;
+    double totalWaktuMenit;
+    double totalWaktuDetik;
+    double totalJarak;
 
+    // Fungsi untuk menghitung pace
+double hitungPace(double totalWaktuMenit, double totalWaktuDetik, double totalJarak) {
+    double totalWaktu = totalWaktuMenit + (totalWaktuDetik / 60.0);
+    return totalWaktu / totalJarak;
+}
+
+// Fungsi untuk menghitung kecepatan (speed) dalam satuan detik
+double hitungKecepatanDetik(double totalWaktuMenit, double totalWaktuDetik, double totalJarak) {
+    double totalWaktuDetikTotal = (totalWaktuMenit * 60) + totalWaktuDetik;
+    return totalJarak / totalWaktuDetikTotal; // dalam detik per kilometer
+}
+}
+
+
+int main() {
 
     int pilihan;
     do {
@@ -13,17 +34,15 @@ int main() {
         cout << "2. Tampilkan hasil perhitungan" << endl;
         cout << "3. Keluar" << endl;
         cout << "Masukkan pilihan Anda: ";
-        return 0;
         cin >> pilihan;
-
         switch (pilihan) {
             case 1: {
-
+                
                 cout << "" << endl;
                 break;
             }
             case 2: {
-
+                
                 cout << "" << endl;
                 break;
             }
@@ -34,6 +53,5 @@ int main() {
                 cout << "Pilihan tidak valid. Silakan coba lagi." << endl;
         }
     } while (pilihan != 3);
-
     return 0;
 }
