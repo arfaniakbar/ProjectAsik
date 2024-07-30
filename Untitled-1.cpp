acb117485520d2412491b4022f7ed76b250d01d1
arfaniakbar <akbarhansem01@gmail.com>
2024-07-30 20:40:17 +0700

    add : penambahan menu awal

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,8 +1,19 @@
+#include<iostream>
 #include <iostream>
-using namespace std 
+#include <vector>
+#include <string>
+using namespace std;
 
 int main() {
-    cout << "Hello, World!";
-    cout << "Hello i am poliban, i am a student"
+    cout << "Hello, World!" << endl;
     return 0;
-}
+
+
+    do {
+        cout << "=== Program Penghitung Pace Lari ===" << endl;
+        cout << "1. Input data lari" << endl;
+        cout << "2. Tampilkan hasil perhitungan" << endl;
+        cout << "3. Keluar" << endl;
+        cout << "Masukkan pilihan Anda: ";
+        return 0;
+    }
